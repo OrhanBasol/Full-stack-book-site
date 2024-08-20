994433e49858dae7fd23ea82461ab2f5b612d616
Orhan <AhmetOrhanBasol@outlook.com>
2024-08-20 19:56:03 +0300

    Finished Author Routes

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Book = require('./book')
 
 const authorSchema = new mongoose.Schema({
     name: {
@@ -6,5 +7,21 @@ const authorSchema = new mongoose.Schema({
         required: true
     }
 })
+
+authorSchema.pre( "deleteOne", { document: true, query: false }, async function (next) {
+      try {
+        const books = await Book.find({ author: this._id }).exec();
+        console.log(books);
+        if (books.length > 0) {
+          next(new Error("This author still has books"));
+        } else {
+          next();
+        }
+      } catch (error) {
+        console.log(error);
+        next(error);
+      }
+    }
+  );
 
 module.exports = mongoose.model('Author', authorSchema)
